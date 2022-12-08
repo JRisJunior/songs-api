00ea97d8fb5dc6754a01613a12eec16f65a7aea0
JRisJunior <jeffmrubin@gmail.com>
2022-12-08 12:34:07 -0600

    start create action and fix indents

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,26 @@
 class SongsController < ApplicationController
-def index
-  songs = Song.all
-  render json: songs.as_json
-end
+  def index
+    songs = Song.all
+    render json: songs.as_json
+  end
 
-def show
-  song = Song.find_by(id: params[:id])
-  render json: song.as_json
-end
+  def show
+    song = Song.find_by(id: params[:id])
+    render json: song.as_json
+  end
 
+  def create
+    
+  end
+
+  def update
+    song = Song.find_by(id: params[:id])
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.artist = params[:artist] || song.arist
+    song.year = params[:year] || song.year
+    render json: {message: "hello from update"}
+  end
 
 end
 
@@ -17,3 +29,4 @@ end
 # get "/songs/:id" => "songs#show"
 # patch "songs/:id" => "songs#update"
 # delete "/songs/:id" => "songs#destroy"
+# post "/songs" => "songs#create"
