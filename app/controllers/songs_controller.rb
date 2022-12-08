f5f3dbd2dd19c8e3a845903c2d68c99f4b1a2f2c
JRisJunior <jeffmrubin@gmail.com>
2022-12-08 12:20:14 -0600

    add RESTful paths + test index render

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,15 @@
 class SongsController < ApplicationController
+def index
+  songs = Song.all
+  render json: songs.as_json
 end
+
+
+
+end
+
+
+# get "/songs" => "songs#index"
+# get "/songs/:id" => "songs#show"
+# patch "songs/:id" => "songs#update"
+# delete "/songs/:id" => "songs#destroy"
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/songs" => "songs#index"
+  get "/songs/:id" => "songs#show"
+  patch "songs/:id" => "songs#update"
+  delete "/songs/:id" => "songs#destroy"
 end
